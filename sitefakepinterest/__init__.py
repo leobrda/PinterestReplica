31e3071e86a27d70ff28b9a57baed81dd128427f
leobrda <leonardo-breda1@hotmail.com>
2024-06-27 05:00:17 -0300

    ajuste init

--- sitefakepinterest/__init__.py
+++ sitefakepinterest/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
