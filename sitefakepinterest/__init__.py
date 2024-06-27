694f8b0f2c9957f7c11cd26110b8a4a8a22f5216
leobrda <leonardo-breda1@hotmail.com>
2024-06-27 04:41:22 -0300

    update init

--- sitefakepinterest/__init__.py
+++ sitefakepinterest/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 app.config['SECRET_KEY'] = 'c53a793f9d0ff75e3ea4cd333085da05'
 app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
 
